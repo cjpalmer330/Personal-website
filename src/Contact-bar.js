38d6eaccabcd1e39d1665d5fdbe1615cfe178377
cjpalmer330 <143898088+cjpalmer330@users.noreply.github.com>
2024-11-02 13:01:41 -0500

    maybe fixed download?

--- src/Contact-bar.js
+++ src/Contact-bar.js
@@ -8,18 +8,17 @@ export default function ContactBar(){
         <a 
         href="https://github.com/cjpalmer330"
         target="_blank"
-        class="border text-center rounded-2xl bg-slate-600 pt-0.5"
+        class="border border-blue-400 text-center rounded-2xl bg-slate-600 pt-0.5"
         >
           Github
         </a>
         <a 
         href="https://www.linkedin.com/in/christopher-palmer-bb48a6253/"
         target="_blank"
-        class="border text-center rounded-2xl bg-slate-600 pt-0.5"
+        class="border border-blue-400 text-center rounded-2xl bg-slate-600 pt-0.5"
         >
           LinkedIn
         </a>
-        <a href='./test.txt' download>Download Resume</a>
       </div>
     </div>
   );
