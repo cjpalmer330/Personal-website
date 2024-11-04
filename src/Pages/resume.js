ae0ed065e2ddde486d01719ff9b8c531662ea6fe
cjpalmer330 <143898088+cjpalmer330@users.noreply.github.com>
2024-11-04 06:20:24 -0600

    trying iframes

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Resume() {
   return(
     <div>
-      <embed src="https://chris-palmer.me/resume-v4.pdf" class="w-screen h-screen"/>
+      <iframe src="resume-v4.pdf" class="w-screen h-screen"></iframe>
     </div>
   );
 }
