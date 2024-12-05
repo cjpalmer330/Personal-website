92b6eac8fe1d3c34fece13e6258694b2fa9d5c9b
cjpalmer330 <143898088+cjpalmer330@users.noreply.github.com>
2024-12-05 08:57:05 -0600

    /preview on resume for instant opening?

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Resume() {
   return(
     <div className='h-screen'>
-      <iframe src="https://drive.google.com/file/d/1mUFwe3De5k9BPpZdQxFnv1XJL5hb0DQA/view?usp=sharing" className="w-full h-full border-none"></iframe>
+      <iframe src="https://drive.google.com/file/d/1mUFwe3De5k9BPpZdQxFnv1XJL5hb0DQA/preview" className="w-full h-full border-none"></iframe>
     </div>
   );
 }
